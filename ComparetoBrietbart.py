b1e9184b4e55d4d5cc2d16ab76679d74df95a33f
Mandi Traud <atraud@cybraics.com>
2017-02-26 19:15:20 -0500

    Commented in instructions

--- ComparetoBrietbart.py
+++ ComparetoBrietbart.py
@@ -3,3 +3,8 @@ from pyspark.sql import SQLContext, HiveContext, Row
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
 from pyspark.mllib.feature import Word2Vec
+
+#Pull in data here were each sentence is a row, split by word, average all sentences in article to get vector for article, and make average vector for input, calculate cosine similarity between input vector and article vector 
+
+word2vec = Word2Vec()
+model = word2vec.fit(inp)
